7974a9088bef19f70e999e36c38c13fde2611178
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2024-08-29 01:49:26 -0400

    basic setup done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,61 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./sections/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		screens: {
+			sm: "375px",
+			md: "768px",
+			lg: "1200px",
+		},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: "1rem",
+				md: "2rem",
+			},
+		},
+		extend: {
+			fontFamily: {
+				sans: ["var(--font-sans)"],
+				serif: ["var(--font-serif)"],
+			},
+			keyframes: {
+				scroll: {
+					"0%": { transform: "translateX(0)" },
+					"100%": { transform: "translateX(-100%)" }, // Adjust as needed
+				},
+				"scroll-back": {
+					to: {
+						transform: "translate(calc(50% + 0.5rem))",
+					},
+				},
+				shake: {
+					"0%": { transform: "translateX(0)" },
+					"25%": { transform: "translateX(-5px)" },
+					"50%": { transform: "translateX(0)" },
+					"75%": { transform: "translateX(5px)" },
+					"100%": { transform: "translateX(0)" },
+				},
+				tapeScroll: {
+					"0%": { transform: "translateX(0)" },
+					"100%": { transform: "translateX(-50%)" },
+				},
+			},
+			animation: {
+				scroll: "scroll var(--animation-duration, 15s) linear infinite",
+				"scroll-back":
+					"scroll-back var(--animation-duration, 15s) var(--animation-direction, forwards) linear infinite",
+				shake: "shake 0.5s ease-in-out",
+				tapeScroll: "tapeScroll 20s linear infinite",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
