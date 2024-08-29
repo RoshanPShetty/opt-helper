ea2725786ec198e9b9f539be2cceb5e9a15600dc
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2024-08-29 14:21:47 -0400

    Implemented Header

---
+++ data/index.ts
@@ -0,0 +1,41 @@
+export const header = [
+	{
+		title: "Home",
+		path: "#home",
+	},
+	{
+		title: "Features",
+		path: "#features",
+	},
+	{
+		title: "Reviews",
+		path: "#reviews",
+	},
+	{
+		title: "Contact",
+		path: "#contact",
+	},
+];
+
+export const socials = [
+	{
+		id: 1,
+		name: "Github",
+		link: "https://github.com/roshanpshetty",
+	},
+	{
+		id: 2,
+		name: "LinkedIn",
+		link: "https://www.linkedin.com/in/roshanpshetty/",
+	},
+	{
+		id: 3,
+		name: "Twitter",
+		link: "https://x.com/roshanpshetty",
+	},
+	{
+		id: 4,
+		name: "Privacy",
+		link: "/privacy-policy",
+	},
+];
